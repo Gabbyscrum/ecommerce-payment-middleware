9452d88d4a0e7bfd19a53e5e9ad8b68de6a476b4
Olaade Gabriel <olaadegabriel@gmail.com>
2025-05-17 08:19:07 +0000

    added memory layer

---
+++ ecommerce-payment-middleware/storage/memory.py
@@ -0,0 +1,11 @@
+from app.storage.repository import Repository
+
+class InMemoryRepository(Repository):
+    def __init__(self):
+        self.store = {}
+
+    def get(self, key):
+        return self.store.get(key)
+
+    def set(self, key, value):
+        self.store[key] = value
